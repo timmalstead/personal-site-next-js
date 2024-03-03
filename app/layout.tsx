5ea202012f14b18b4b860189313102210fe9360d
timmalstead <timmalstead@gmail.com>
2024-03-02 16:31:10 -0800

    adds prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,23 @@
-import type { Metadata } from "next";
-import { Noto_Sans } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from "next"
+import { Noto_Sans } from "next/font/google"
+import "./globals.css"
 
-const notoSans = Noto_Sans({ subsets: ["latin"] });
+const notoSans = Noto_Sans({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: {template: '%s | Timothy Malstead', default: 'Timothy Malstead'},
-  description: 'The website of Timothy Malstead, a software engineer and artist from the United States.',
-};
+    title: { template: "%s | Timothy Malstead", default: "Timothy Malstead" },
+    description:
+        "The website of Timothy Malstead, a software engineer and artist from the United States.",
+}
 
- 
-const RootLayout =({
-  children,
+const RootLayout = ({
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode
 }>) => (
     <html lang="en">
-      <body className={notoSans.className}>{children}</body>
+        <body className={notoSans.className}>{children}</body>
     </html>
-  )
+)
 
 export default RootLayout
