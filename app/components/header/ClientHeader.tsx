bc2a8736be1a305248ccb4baf3e0c33df91b5bc7
timmalstead <timmalstead@gmail.com>
2024-03-14 23:20:52 -0700

    simple intersecton observer sticky header

--- app/components/header/ClientHeader.tsx
+++ app/components/header/ClientHeader.tsx
@@ -1,14 +1,33 @@
 "use client"
 import type { ReactNode } from "react"
-import { useEffect } from "react"
+import { useLayoutEffect, useState, useRef } from "react"
+import styles from "./ClientHeader.module.css"
 
 // use intersection observer && figure out if mouse is going down or up
+// could i make the initial use state a query to the window
 const ClientHeader = ({ children }: { children: ReactNode }) => {
-    useEffect(() => {
-        console.log("ClientHeader is on the client")
-        console.log(children)
-    }, [children])
-    return <header>{children}</header>
+    const [positionClass, setPositionClass] = useState<"static" | "sticky">(
+        "static"
+    )
+    const headerRef = useRef<HTMLHRElement | null>(null)
+    useLayoutEffect(() => {
+        if (headerRef.current) {
+            const observer = new IntersectionObserver(
+                ([{ isIntersecting }]) => {
+                    setPositionClass(isIntersecting ? "static" : "sticky")
+                }
+            )
+            observer.observe(headerRef.current)
+            return () => observer.disconnect()
+        }
+    }, [headerRef])
+
+    return (
+        <>
+            <header className={styles[positionClass]}>{children}</header>
+            <hr ref={headerRef} className={styles.spacer} />
+        </>
+    )
 }
 
 export default ClientHeader
