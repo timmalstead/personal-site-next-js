0fcc3ebc3c52ceaff6e2dcf21c1c78a046549105
timmalstead <timmalstead@gmail.com>
2024-03-15 18:22:20 -0700

    completes fun sliding header

--- app/components/header/ClientHeader.tsx
+++ app/components/header/ClientHeader.tsx
@@ -1,33 +1,70 @@
 "use client"
-import type { ReactNode } from "react"
-import { useLayoutEffect, useState, useRef } from "react"
+// scroll direction hook adapted from Robin Wieruch's blog: https://www.robinwieruch.de/react-hook-scroll-direction/
+// maybe only display the switcher if js is enabled?
+import { ReactNode, useLayoutEffect, useState, useRef, useEffect } from "react"
 import styles from "./ClientHeader.module.css"
 
-// use intersection observer && figure out if mouse is going down or up
-// could i make the initial use state a query to the window
-// maybe only display the switcher if js is enabled?
-// for links, change text decoration style to underline on hover?
+type Direction = "up" | "down"
+type Position = "show" | "hide"
+
+const THRESHOLD = 0
 const ClientHeader = ({ children }: { children: ReactNode }) => {
-    const [positionClass, setPositionClass] = useState<"static" | "sticky">(
-        "static"
-    )
-    const headerRef = useRef<HTMLHRElement | null>(null)
+    const [currentScrollDir, setCurrentScrollDir] = useState<Direction>("up")
+
+    const blocking = useRef<boolean>(false)
+    const prevScrollY = useRef<number>(0)
+
+    useEffect(() => {
+        prevScrollY.current = window.scrollY
+
+        const updateScrollDirection = () => {
+            const scrollY = window.scrollY
+
+            if (Math.abs(scrollY - prevScrollY.current) >= THRESHOLD) {
+                const newScrollDirection =
+                    scrollY > prevScrollY.current ? "down" : "up"
+
+                setCurrentScrollDir(newScrollDirection)
+
+                prevScrollY.current = scrollY > 0 ? scrollY : 0
+            }
+
+            blocking.current = false
+        }
+
+        const onScroll = () => {
+            if (!blocking.current) {
+                blocking.current = true
+                requestAnimationFrame(updateScrollDirection)
+            }
+        }
+
+        addEventListener("scroll", onScroll)
+
+        return () => removeEventListener("scroll", onScroll)
+    }, [currentScrollDir])
+
+    const [positionClass, setPositionClass] = useState<Position>("show")
+    const hrRef = useRef<HTMLHRElement | null>(null)
     useLayoutEffect(() => {
-        if (headerRef.current) {
+        if (hrRef.current) {
             const observer = new IntersectionObserver(
                 ([{ isIntersecting }]) => {
-                    setPositionClass(isIntersecting ? "static" : "sticky")
+                    if (isIntersecting || currentScrollDir === "up")
+                        setPositionClass("show")
+                    else if (!isIntersecting && currentScrollDir === "down")
+                        setPositionClass("hide")
                 }
             )
-            observer.observe(headerRef.current)
+            observer.observe(hrRef.current)
             return () => observer.disconnect()
         }
-    }, [headerRef])
+    }, [hrRef, currentScrollDir])
 
     return (
         <>
             <header className={styles[positionClass]}>{children}</header>
-            <hr ref={headerRef} className={styles.spacer} />
+            <hr ref={hrRef} className={styles.hr} />
         </>
     )
 }
