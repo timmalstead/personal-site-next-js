94ee1dce208a0a9ba8ff2b21a1b2fa003036df08
timmalstead <timmalstead@gmail.com>
2024-04-15 22:32:18 -0700

    adds tests for color switcher

--- e2e/common.spec.ts
+++ e2e/common.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from "@playwright/test"
+import { test, expect, Page } from "@playwright/test"
 
 test("Header should be sticky when scrolling up, but not down", async ({
     page,
@@ -12,53 +12,81 @@ test("Header should be sticky when scrolling up, but not down", async ({
 
     await page.mouse.wheel(0, -1000)
     await expect(headerLink).toBeInViewport()
-    page.close()
+    await page.close()
 })
 
-const [darkColor, lightColor] = ["rgb(27, 25, 25)", "rgb(230, 230, 230)"] // #1b1919 and #e6e6e6 respectively
-const loadingColorTestCases = [
-    {
-        testName: "Has dark background with dark colorMode cookie",
-        cookieName: "colorMode",
-        cookieValue: "dark",
-        headerName: "",
-        headerValue: "",
-        expectedColor: darkColor,
-    },
-    {
-        testName: "Has light background with light colorMode cookie",
-        cookieName: "colorMode",
-        cookieValue: "light",
-        headerName: "",
-        headerValue: "",
-        expectedColor: lightColor,
-    },
-    {
-        testName: "Has light background with light header",
-        cookieName: "",
-        cookieValue: "",
-        headerName: "Sec-CH-Prefers-Color-Scheme",
-        headerValue: "light",
-        expectedColor: lightColor,
-    },
-    /* TODO: Fix this test {
-        testName: "Has dark background with dark header",
-        cookieName: "",
-        cookieValue: "",
-        headerName: "Sec-CH-Prefers-Color-Scheme",
-        headerValue: "dark",
-        expectedColor: darkColor,
-    }, */
-    {
-        testName: "Has light background by default",
-        cookieName: "",
-        cookieValue: "",
-        headerName: "",
-        headerValue: "",
-        expectedColor: lightColor,
-    },
-]
-test.describe("loading color theme tests", () => {
+test.describe("Color themes", () => {
+    const [darkColor, lightColor] = ["rgb(27, 25, 25)", "rgb(230, 230, 230)"] // #1b1919 and #e6e6e6 respectively
+    const bgShouldBe = async (page: Page, color: string) =>
+        expect(page.getByTestId("background")).toHaveCSS(
+            "background-color",
+            color
+        )
+
+    // how can we test the media query inside the color switcher?
+
+    test("Color theme should switch when color switcher is clicked", async ({
+        page,
+    }) => {
+        await page.goto("/")
+
+        await bgShouldBe(page, lightColor)
+
+        const settings = page.getByText("settings")
+        await settings.click()
+
+        const colorSwitcher = page.getByText(/theme/)
+
+        await colorSwitcher.click()
+        await bgShouldBe(page, darkColor)
+
+        await colorSwitcher.click()
+        await bgShouldBe(page, lightColor)
+        await page.close()
+    })
+
+    const loadingColorTestCases = [
+        {
+            testName: "Has dark background with dark colorMode cookie",
+            cookieName: "colorMode",
+            cookieValue: "dark",
+            headerName: "",
+            headerValue: "",
+            expectedColor: darkColor,
+        },
+        {
+            testName: "Has light background with light colorMode cookie",
+            cookieName: "colorMode",
+            cookieValue: "light",
+            headerName: "",
+            headerValue: "",
+            expectedColor: lightColor,
+        },
+        {
+            testName: "Has light background with light header",
+            cookieName: "",
+            cookieValue: "",
+            headerName: "Sec-CH-Prefers-Color-Scheme",
+            headerValue: "light",
+            expectedColor: lightColor,
+        },
+        /* TODO: Fix this test {
+            testName: "Has dark background with dark header",
+            cookieName: "",
+            cookieValue: "",
+            headerName: "Sec-CH-Prefers-Color-Scheme",
+            headerValue: "dark",
+            expectedColor: darkColor,
+        }, */
+        {
+            testName: "Has light background by default",
+            cookieName: "",
+            cookieValue: "",
+            headerName: "",
+            headerValue: "",
+            expectedColor: lightColor,
+        },
+    ]
     for (const {
         testName,
         cookieName,
@@ -70,27 +98,26 @@ test.describe("loading color theme tests", () => {
         test(testName, async ({ browser }) => {
             const context = await browser.newContext()
 
-            cookieName &&
-                cookieValue &&
-                (await context.addCookies([
+            if (cookieName && cookieValue)
+                await context.addCookies([
                     {
                         name: cookieName,
                         value: cookieValue,
                         url: "http://127.0.0.1:3000/",
                     },
-                ]))
+                ])
+
             const page = await context.newPage()
 
-            headerName &&
-                headerValue &&
-                (await page.setExtraHTTPHeaders({ [headerName]: headerValue }))
+            if (headerName && headerValue)
+                await page.setExtraHTTPHeaders({
+                    [headerName]: headerValue,
+                })
 
             await page.goto("/")
-            const background = page.getByTestId("background")
-            await expect(background).toHaveCSS(
-                "background-color",
-                expectedColor
-            )
+            await bgShouldBe(page, expectedColor)
+            await page.close()
+            await context.close()
         })
     }
 })
