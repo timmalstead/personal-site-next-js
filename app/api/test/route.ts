cc59d1ce6f42e8d7564ec4c8cc4ff332fbc31203
timmalstead <timmalstead@gmail.com>
2024-07-15 17:26:31 -0700

    fixes mobile heading size and adds better type checking

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,11 +1,12 @@
 import { firestoreDatabase } from "../../_utils/firestore"
+import { NextRequest, NextResponse } from "next/server"
 
 export const dynamic = "force-dynamic"
-export const GET = async (request: Request) => {
+export const GET = async (request: NextRequest) => {
     const docRef = firestoreDatabase.doc("users/alovelace")
     const firestoreData = await docRef.get().then((doc) => doc.data())
 
-    return Response.json({
+    return NextResponse.json({
         hello: "from the api",
         request: JSON.stringify(request),
         firestoreData,
