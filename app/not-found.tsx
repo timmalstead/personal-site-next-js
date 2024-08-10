9799830cc01a15525da68894a25b3534d8fe3037
timmalstead <timmalstead@gmail.com>
2024-08-10 11:24:28 -0400

    refactors not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,8 +1,22 @@
-import "./not-found.css"
-const Custom404 = () => (
+/* prettier-ignore */
+const errorStyle = `
+    main.not-found {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        h1 {
+            font-weight: var(--heading-weight);
+            border-bottom: 0.075rem solid;
+            text-align: center;
+        }
+    }
+`
+
+const NotFound = () => (
     <main className="not-found">
+        <style>{errorStyle}</style>
         <h1>404 - Page Not Found</h1>
     </main>
 )
 
-export default Custom404
+export default NotFound
