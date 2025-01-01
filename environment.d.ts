84d51a3008aa22a102831e814736bf24056fec84
timmalstead <timmalstead@gmail.com>
2025-01-01 11:40:37 -0800

    adds typing for node env vars

---
+++ environment.d.ts
@@ -0,0 +1,15 @@
+/// <reference types="node" />
+
+declare namespace NodeJS {
+    interface ProcessEnv {
+        readonly ACTIVE_ENV: "local" | "preview" | "prod"
+        readonly USE_TESTING_FIRESTORE: "true" | "false"
+        readonly CI: "true" | "false"
+        readonly PORT: string
+        readonly MIDDLEWARE_AUTHORIZATION: string
+        readonly PROJECT_ID: string
+        readonly SERVICE_ACCT_CLIENT_EMAIL: string
+        readonly SERVICE_ACCT_PRIVATE_KEY: string
+        readonly CURRENT_VERSION: string
+    }
+}
