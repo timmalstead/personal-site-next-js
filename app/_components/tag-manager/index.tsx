d11a4e0d7a40a76ec6cd0462c11a735ce9e017fc
timmalstead <timmalstead@gmail.com>
2025-01-01 11:43:54 -0800

    loads tag manager for prod env only

--- app/_components/tag-manager/index.tsx
+++ app/_components/tag-manager/index.tsx
@@ -1,23 +1,25 @@
 import Script from "next/script"
 
 const gTagID = "G-6YHZM21N8W"
-const tagManagerImplementation = `
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', '${gTagID}');
-`
-const TagManager = () => (
-    <>
-        <Script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${gTagID}`}
-            strategy="afterInteractive"
-        />
-        <Script id="gtag" strategy="afterInteractive">
-            {tagManagerImplementation}
-        </Script>
-    </>
-)
+
+const shouldLoadTagManager =
+    process.env?.NODE_ENV === "production" && process.env?.ACTIVE_ENV === "prod"
+
+const TagManager = () =>
+    shouldLoadTagManager && (
+        <>
+            <Script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${gTagID}`}
+                strategy="afterInteractive"
+            />
+            <Script id="gtag" strategy="afterInteractive">
+                {`window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${gTagID}');`}
+            </Script>
+        </>
+    )
 
 export default TagManager
