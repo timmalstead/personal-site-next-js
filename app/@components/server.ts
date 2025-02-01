b67777944519fb1e4f6187d5034ebd1c9c9f8f57
timmalstead <timmalstead@gmail.com>
2025-02-01 14:19:56 -0800

    dedupes type exports in app/@components/server.ts

--- app/@components/server.ts
+++ app/@components/server.ts
@@ -3,18 +3,17 @@ export { default as Header } from "./header"
 export { default as Markdown } from "./markdown"
 export { default as Heading } from "./heading"
 export { default as Resolver } from "./resolver"
-export { default as Image } from "./image"
-export type { ImageProps } from "./image"
-export { default as LastModified } from "./last-modified"
-export type { LastModifiedProps } from "./last-modified"
-export { default as Link } from "./link"
-export type { LinkProps } from "./link"
+export { default as Image, type ImageProps } from "./image"
+export {
+    default as LastModified,
+    type LastModifiedProps,
+} from "./last-modified"
+export { default as Link, type LinkProps } from "./link"
 export {
     default as DynamicPageComponent,
     generateMetadata,
 } from "./dynamic-page-component"
-export { default as ObjectComponent } from "./object"
-export type { ObjectComponentProps } from "./object"
+export { default as ObjectComponent, type ObjectComponentProps } from "./object"
 export { default as TagManager } from "./tag-manager"
 export { default as CurrentVersion } from "./current-version"
 export { default as Code } from "./code"
