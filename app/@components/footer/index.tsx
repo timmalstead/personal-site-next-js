8bd90dc837be1ba4f275fb6f3b384d5b5e08bb5d
timmalstead <timmalstead@gmail.com>
2025-02-03 21:46:39 -0800

    refactors footer links a bit

--- app/@components/footer/index.tsx
+++ app/@components/footer/index.tsx
@@ -1,3 +1,4 @@
+import type { JSX } from "react"
 import {
     Resolver as resolver,
     Link,
@@ -7,27 +8,37 @@ import {
     type LogoProps,
 } from "@components/server"
 
+type LogoLinkProps = {
+    key: string
+    href: string
+    Logo: (props: LogoProps) => JSX.Element
+} & LogoProps
+
 const copyRight = `© 2019 - ${new Date().getFullYear()} by Timothy Malstead - All Rights Reserved`
 
-const sharedProps = { width: "20", height: "20", fill: "var(--text-color)" }
-const logos = [
+const sharedProps: LogoProps = {
+    width: "20",
+    height: "20",
+    fill: "var(--text-color)",
+}
+const logos: LogoLinkProps[] = [
     {
         ...sharedProps,
         key: "Email",
         href: "mailto:timmalstead@gmail.com",
-        Logo: (props: LogoProps) => <EmailLogo {...props} />,
+        Logo: (props) => <EmailLogo {...props} />,
     },
     {
         ...sharedProps,
         key: "Mastodon",
         href: "https://mastodon.social/@timothymalstead",
-        Logo: (props: LogoProps) => <MastodonLogo {...props} />,
+        Logo: (props) => <MastodonLogo {...props} />,
     },
     {
         ...sharedProps,
         key: "Bluesky",
         href: "https://bsky.app/profile/timmalstead.bsky.social",
-        Logo: (props: LogoProps) => <BlueskyLogo {...props} />,
+        Logo: (props) => <BlueskyLogo {...props} />,
     },
 ]
 
@@ -38,7 +49,7 @@ const Footer = async () => {
     })
     return (
         <footer>
-            {Boolean(footerLinks?.length) && (
+            {Boolean(footerLinks?.length || logos?.length) && (
                 <nav>
                     <ul>
                         {footerLinks?.map((link) => (
