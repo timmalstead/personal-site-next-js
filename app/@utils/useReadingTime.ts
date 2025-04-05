9fc8af00475f980823cd637c4e3cc544797c0a26
timmalstead <timmalstead@gmail.com>
2025-04-05 12:59:21 -0700

    adds js doc for use reading time hook

--- app/@utils/useReadingTime.ts
+++ app/@utils/useReadingTime.ts
@@ -4,6 +4,15 @@ import { useEffect, useState } from "react"
 const AVERAGE_READING_SPEED = 240 // words per minute
 const [NEWLINES, SPACES] = [/\n+/g, /\s+/]
 
+/**
+ * Custom React hook to calculate the estimated reading time for the text content
+ * of an HTML element selected by a CSS selector.
+ *
+ * @param {string} cssSelector - The CSS selector of the HTML element whose text content
+ *                               will be used to calculate the reading time.
+ * @returns {number | null} - The estimated reading time in minutes, or null if the text
+ *                            content is not available or cannot be calculated.
+ */
 export const useReadingTime = (cssSelector: string) => {
     const [readingTime, setReadingTime] = useState<number | null>(null)
     useEffect(() => {
